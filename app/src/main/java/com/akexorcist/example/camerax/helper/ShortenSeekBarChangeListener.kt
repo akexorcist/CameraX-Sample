24039046ad57281bbb9fe0df79414b57ccb3d728
Akexorcist <akexorcist@gmail.com>
2020-04-02 22:26:54 +0700

    Add shorten seek bar changed listener

---
+++ app/src/main/java/com/akexorcist/example/camerax/helper/ShortenSeekBarChangeListener.kt
@@ -0,0 +1,14 @@
+package com.akexorcist.example.camerax.helper
+
+import android.widget.SeekBar
+
+open class ShortenSeekBarChangeListener : SeekBar.OnSeekBarChangeListener {
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+    }
+}
